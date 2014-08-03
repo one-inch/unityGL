71a6d65144a87c37f941063a15197301cdc86d41
one-inch <wangxiaona@wangxiaonadeMacBook-Pro.local>
2014-08-03 19:04:13 +0800

    unity GL and Mesh
    create my test

---
+++ MyGLDraw.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyGLDraw : MonoBehaviour 
+{
+	public Material material = null;
+
+	void OnPostRender()
+	{
+		GL.PushMatrix();
+		material.SetPass(0);
+		GL.LoadOrtho();
+		GL.Begin(GL.QUADS);
+		GL.TexCoord2(0, 0);
+		GL.Vertex3(0, 0, 0);
+		GL.TexCoord2(0, 1);
+		GL.Vertex3(0, 1, 0);
+		GL.TexCoord2(1, 1);
+		GL.Vertex3(1, 1, 0);
+		GL.TexCoord2(1, 0);
+		GL.Vertex3(1, 0, 0);
+		GL.End();
+		GL.PopMatrix();
+	}
+}
---
+++ MyMeshDraw.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMeshDraw : MonoBehaviour
+{
+	public Vector2[] uvs;
+	public Vector3[] verts;
+	public int[] trangles;
+	private MeshFilter meshFilter = null;
+	public Material material = null;
+	public Mesh mesh = null;
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		meshFilter = GetComponent<MeshFilter>();
+		this.mesh = new Mesh();
+		this.meshFilter.mesh = this.mesh;
+		GetComponent<MeshRenderer>().material = this.material;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.meshFilter.mesh.vertices = verts;
+		this.meshFilter.mesh.uv = uvs;
+		this.meshFilter.mesh.triangles = trangles;
+	}
+}
